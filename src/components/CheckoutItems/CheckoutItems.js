a78abc1dbac83ca08343929137c90e3f4be5479f
jessemcc <jesse.mcconnell@live.ca>
2023-02-15 20:55:56 -0500

    implemented stripe and moved the shipping info to seperate page

--- src/components/CheckoutItems/CheckoutItems.js
+++ src/components/CheckoutItems/CheckoutItems.js
@@ -1,48 +1,6 @@
 import "./CheckoutItems.scss";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
-const CheckoutItems = () => {
-  const [cart, setCart] = useState([]);
-  const [deletedId, setDeletedId] = useState(null);
-  const { REACT_APP_API_URL } = process.env;
-
-  // GET ALL CART ITEMS =======================================================
-  useEffect(() => {
-    try {
-      const getCartItems = async () => {
-        const { data } = await axios.get(`${REACT_APP_API_URL}/cart`);
-        setCart(data);
-      };
-      getCartItems();
-    } catch (error) {
-      console.error(error);
-    }
-  }, [deletedId]);
-
-  if (!cart) {
-    <p>Loading...</p>;
-  }
-
-  // GET TOTAL PRICE OF ALL CART ITEMS TOGETHER ===================================
-  const getTotalPrice = (arr) => {
-    let currentTotal = 0;
-    arr.map((item) => {
-      currentTotal += item.price;
-    });
-    return currentTotal.toFixed(2);
-  };
-  const currentTotal = getTotalPrice(cart);
-
-  // DELETE CART ITEM ============================================================
-  const handleDelete = async (id) => {
-    await axios.delete(`${REACT_APP_API_URL}/cart`, {
-      data: { id },
-    });
-    setDeletedId(id);
-  };
-
+const CheckoutItems = ({ cart, handleDelete, currentTotal }) => {
   const itemsList = cart.map((item) => {
     return (
       <section className="cart-item__container" key={item.id}>
